39ac2ff8f3e8c58c52db900c23598ae88e6e6b3b
Abey Onalaja <abey.onalaja@gmail.com>
2019-07-05 17:22:27 +0100

    revert migrate task

--- lib/example/tasks/migrate.ex
+++ lib/example/tasks/migrate.ex
@@ -1,86 +1,38 @@
 defmodule Example.Tasks.Migrate do
-  @start_apps [
-    :crypto,
-    :ssl,
-    :postgrex,
-    :ecto,
-    # If using Ecto 3.0 or higher
-    :ecto_sql
-  ]
+  @moduledoc false
 
-  @repos Application.get_env(:my_app, :ecto_repos, [])
+  def migrate(_args) do
+    # Configure
+    Mix.Releases.Config.Providers.Elixir.init(["${RELEASE_ROOT_DIR}/etc/config.exs"])
+    repo_config = Application.get_env(:distillery_example, Example.Repo)
+    repo_config = Keyword.put(repo_config, :adapter, Ecto.Adapters.Postgres)
+    Application.put_env(:distillery_example, Example.Repo, repo_config)
 
-  def migrate(_argv) do
-    start_services()
-
-    run_migrations()
-
-    stop_services()
-  end
-
-  def seed(_argv) do
-    start_services()
-
-    run_migrations()
-
-    run_seeds()
-
-    stop_services()
-  end
-
-  defp start_services do
-    IO.puts("Starting dependencies..")
-    # Start apps necessary for executing migrations
-    Enum.each(@start_apps, &Application.ensure_all_started/1)
-
-    # Start the Repo(s) for app
-    IO.puts("Starting repos..")
-
-    # pool_size can be 1 for ecto < 3.0
-    Enum.each(@repos, & &1.start_link(pool_size: 2))
-  end
-
-  defp stop_services do
-    IO.puts("Success!")
-    :init.stop()
-  end
-
-  defp run_migrations do
-    Enum.each(@repos, &run_migrations_for/1)
-  end
-
-  defp run_migrations_for(repo) do
-    app = Keyword.get(repo.config(), :otp_app)
-    IO.puts("Running migrations for #{app}")
-    migrations_path = priv_path_for(repo, "migrations")
-    Ecto.Migrator.run(repo, migrations_path, :up, all: true)
-  end
-
-  defp run_seeds do
-    Enum.each(@repos, &run_seeds_for/1)
-  end
-
-  defp run_seeds_for(repo) do
-    # Run the seed script if it exists
-    seed_script = priv_path_for(repo, "seeds.exs")
-
-    if File.exists?(seed_script) do
-      IO.puts("Running seed script..")
-      Code.eval_file(seed_script)
+    # Start requisite apps
+    IO.puts "==> Starting applications.."
+    for app <- [:crypto, :ssl, :postgrex, :ecto, :ecto_sql] do
+      {:ok, res} = Application.ensure_all_started(app)
+      IO.puts "==> Started #{app}: #{inspect res}"
     end
-  end
 
-  defp priv_path_for(repo, filename) do
-    app = Keyword.get(repo.config(), :otp_app)
+    # Start the repo
+    IO.puts "==> Starting repo"
+    {:ok, _pid} = Example.Repo.start_link(pool_size: 15, log: :info, log_sql: true)
 
-    repo_underscore =
-      repo
-      |> Module.split()
-      |> List.last()
-      |> Macro.underscore()
+    # Run the migrations for the repo
+    IO.puts "==> Running migrations"
+    priv_dir = Application.app_dir(:distillery_example, "priv")
+    migrations_dir = Path.join([priv_dir, "repo", "migrations"])
 
-    priv_dir = "#{:code.priv_dir(app)}"
+    opts = [all: true]
+    pool = Example.Repo.config[:pool]
+    if function_exported?(pool, :unboxed_run, 2) do
+      pool.unboxed_run(Example.Repo, fn -> Ecto.Migrator.run(Example.Repo, migrations_dir, :up, opts) end)
+    else
+      Ecto.Migrator.run(Example.Repo, migrations_dir, :up, opts)
+    end
 
-    Path.join([priv_dir, repo_underscore, filename])
+    # Shut down
+    :init.stop()
   end
 end
